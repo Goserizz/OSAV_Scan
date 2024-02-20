5acf46ee682de9b2890739dd2b22278f9569d9da
Rafael Lee <goserizz@gmail.com>
2024-02-20 00:14:33 -0500

    scan in reverse TTL

--- tcproute_scan/tcproute_scan.go
+++ tcproute_scan/tcproute_scan.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"os"
+	"context"
 	"fmt"
+	"os"
 	"sync"
 	"time"
-	"context"
 
-	"golang.org/x/time/rate"
 	"github.com/schollz/progressbar/v3"
+	"golang.org/x/time/rate"
 )
 
 const (
 	LOCAL_PORT = 37300
-	BUF_SIZE = 10000
-	BURST = 100
-	LOG_INTV = 10000
+	BUF_SIZE   = 10000
+	BURST      = 100
+	LOG_INTV   = 10000
 )
 
 func TCPRouteScan(srcIpStr, iface, inputFile, outputFile string, startTtl, endTtl uint8, pps int, srcMac, dstMac []byte, remotePort uint16) {
 	os.Remove(outputFile)
 	dstIpStrArray := ReadLineAddr6FromFS(inputFile)
-	bar := progressbar.Default(int64(len(dstIpStrArray) * int(endTtl - startTtl + 1)), "Scanning...")
+	bar := progressbar.Default(int64(len(dstIpStrArray)*int(endTtl-startTtl+1)), "Scanning...")
 	var doneIps sync.Map
 	var testIps sync.Map
 	limiter := rate.NewLimiter(rate.Limit(pps), BURST)
@@ -30,7 +30,8 @@ func TCPRouteScan(srcIpStr, iface, inputFile, outputFile string, startTtl, endTt
 		testIps.Store(dstIpStr, true)
 	}
 	counter := 0
-	for ttl := startTtl; ttl <= endTtl; ttl ++ {
+	tfSet := make(map[string]bool)
+	for ttl := endTtl; ttl >= startTtl; ttl-- {
 		p := NewTCPoolv4(remotePort, BUF_SIZE, LOCAL_PORT, iface, srcIpStr, srcMac, dstMac, ttl)
 		bar.Describe(fmt.Sprintf("Scanning TTL=%d...", ttl))
 		finish := false
@@ -39,8 +40,13 @@ func TCPRouteScan(srcIpStr, iface, inputFile, outputFile string, startTtl, endTt
 		go func() {
 			for _, dstIpStr := range dstIpStrArray {
 				counter += 1
-				if counter % LOG_INTV == 0 { bar.Add(LOG_INTV) }
-				_, ok := doneIps.Load(dstIpStr); if ok { continue }
+				if counter%LOG_INTV == 0 {
+					bar.Add(LOG_INTV)
+				}
+				_, ok := doneIps.Load(dstIpStr)
+				if ok {
+					continue
+				}
 				limiter.Wait(context.TODO())
 				p.Add(dstIpStr)
 			}
@@ -51,14 +57,19 @@ func TCPRouteScan(srcIpStr, iface, inputFile, outputFile string, startTtl, endTt
 		// recieve icmp
 		go func() {
 			for {
-				target, real, _, _ := p.GetIcmp()
+				target, real, res, _ := p.GetIcmp()
 				if target == "" {
-					if finish { break }
-				} else {
-					if _, ok := testIps.Load(target); !ok { continue }
-					if target != real {
-						Append1Addr6ToFS(outputFile, target + "," + real)
+					if finish {
+						break
 					}
+				} else {
+					if _, ok := testIps.Load(target); !ok {
+						continue
+					}
+					if target != real {
+						Append1Addr6ToFS(outputFile, target + "," + real + "," + res)
+						tfSet[target] = true
+					} else if tfSet[target] { Append1Addr6ToFS(outputFile, target + "," + real + "," + res) }
 				}
 			}
 		}()
@@ -68,9 +79,13 @@ func TCPRouteScan(srcIpStr, iface, inputFile, outputFile string, startTtl, endTt
 			for {
 				target, real, _ := p.GetTcp()
 				if target == "" {
-					if finish { break }
+					if finish {
+						break
+					}
 				} else {
-					if _, ok := testIps.Load(target); !ok { continue }
+					if _, ok := testIps.Load(target); !ok {
+						continue
+					}
 					if target != real {
 						// Append1Addr6ToFS(outputFile, target + "," + real)
 						doneIps.Store(target, true)
@@ -79,7 +94,9 @@ func TCPRouteScan(srcIpStr, iface, inputFile, outputFile string, startTtl, endTt
 			}
 		}()
 
-		for !finish { time.Sleep(time.Second) }
+		for !finish {
+			time.Sleep(time.Second)
+		}
 		p.Finish()
 	}
 }
